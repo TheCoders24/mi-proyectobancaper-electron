040687944de2199f8b15ca59bc9fa21e56d990ac
Alexis Urquidez <162842416+TheCoders24@users.noreply.github.com>
2026-02-27 12:49:59 -0600

    add

--- docs/consultas.sql
+++ docs/consultas.sql
@@ -12,3 +12,6 @@ from Sucursales s where s.id_sucursal='SUC-002'
 INSERT INTO EQUIPOS_POR_SUCURSAL(Id_equipo,id_sucurusal, nombre, status_del_registro)
 select NEWID() as Id_equipo , 'SUC-002'   AS id_sucurusal, nombre, status_del_registro
 from EQUIPOS_POR_SUCURSAL
+
+
+select * from Perfiles
